684d48a6efc0295fae4b4adcd87d20396c9746d3
Patricia Medrano <patriciamedrano502@gmail.com>
2026-09-06 20:39:28 +0800

    Homepage

--- app/views/users.php
+++ app/views/users.php
@@ -279,7 +279,7 @@
                 <thead>
                     <tr>
                         <th>ID</th>
-                        <th>First Names</th>
+                        <th>First Name</th>
                         <th>Last Name</th>
                         <th>Email</th>
                         <th>Username</th>
